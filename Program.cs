be31db992ebae70d03810680a625abab78f8a955
Provod1337 <damir07tairov@gmail.com>
2025-05-22 09:17:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿int N = int.Parse(File.ReadAllText("INPUT.TXT"));
+
+        int[,] matrix = new int[N, N];
+        int num = 1;
+
+        for (int col = 0; col < N; col++)
+        {
+            if (col % 2 == 0)
+            {
+                for (int row = 0; row < N; row++)
+                {
+                    matrix[row, col] = num++;
+                }
+            }
+            else
+            {
+                for (int row = N - 1; row >= 0; row--)
+                {
+                    matrix[row, col] = num++;
+                }
+            }
+        }
+
+
+using (StreamWriter sw = new StreamWriter("OUTPUT.TXT"))
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            sw.Write(matrix[i, j] + " ");
+        }
+        sw.WriteLine();
+    }
+}
